f13fe9640ff2dbb6150c8cfeaf1a7463257aa697
ThiagoDuarteC <thiagodcorreia01@gmail.com>
2024-12-30 15:30:14 -0300

    Edit and destroy now works

--- Contacts/src/Main.java
+++ Contacts/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
                     }
                     System.out.println("Seus contatos:");
                     for(int i = 0; i < contatos.size(); i++){
-                        System.out.println( i+1 + " - " + contatos.get(i).getFullName());
+                        System.out.println( i+1 + " - " + contatos.get(i).getFullName() + " (" + contatos.get(i).getPhone() + ")");
                     }
                     break;
                 case 2:
@@ -40,10 +40,57 @@ public class Main {
                     contatos.add(contato);
                     break;
                 case 3:
-                    //Editar contato
+                    if (contatos.isEmpty()){
+                        System.out.println("Não há contatos para editar");
+                        continue;
+                    }
+                    System.out.println("Digite o número referente ao contato que deseja editar");
+                    int indexEdit = Integer.parseInt(read.nextLine());
+                    try {
+                        contato = contatos.get(indexEdit-1);
+                    }
+                    catch (Exception e) {
+                        System.out.println("Não existe um contato referente ao número informado");
+                        continue;
+                    }
+                    System.out.println("""
+                            Escolha o que deseja editar:
+                            1- Nome
+                            2- Telefone
+                            0- Voltar
+                            """);
+                    int opcaoEdit = Integer.parseInt(read.nextLine());
+                    switch (opcaoEdit){
+                        case 1:
+                            System.out.println("Informe o novo nome do seu contato");
+                            String novoNome = read.nextLine();
+                            contato.setFullName(novoNome);
+                            break;
+                        case 2:
+                            System.out.println("Informe o novo número do seu contato");
+                            String novoNumero = read.nextLine();
+                            contato.setPhone(novoNumero);
+                            break;
+                        case 0:
+                            continue;
+                    }
+                    System.out.println("Contato editado com sucesso");
                     break;
                 case 4:
-                    //Excluir contato
+                    if (contatos.isEmpty()){
+                        System.out.println("Não há contatos para excluir");
+                        continue;
+                    }
+                    System.out.println("Digite o número referente ao contato que deseja excluir");
+                    int indexDestroy = Integer.parseInt(read.nextLine());
+                    try {
+                        contatos.remove(indexDestroy -1);
+                    }
+                    catch (Exception e) {
+                        System.out.println("Não existe um contato referente ao número informado");
+                        continue;
+                    }
+                    System.out.println("Contato excluido com sucesso");
                     break;
                 case 0:
                     System.out.println("Adeus!");
